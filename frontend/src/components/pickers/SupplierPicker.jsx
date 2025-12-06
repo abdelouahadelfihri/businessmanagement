e64c0a806d5db0d4aab498a7e66280a2523b7301
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-06 19:36:45 +0100

    updates to SupplierPicker.jsx

--- frontend/src/components/pickers/SupplierPicker.jsx
+++ frontend/src/components/pickers/SupplierPicker.jsx
@@ -1,18 +1,31 @@
 // src/components/pickers/SupplierPicker.jsx
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import EntityPicker from "./EntityPicker";
+import { useDispatch } from "react-redux";
 import { fetchSuppliers, addSupplierThunk } from "../../slices/purchases/supplierSlice";
 
-export default function SupplierPicker(props) {
+export default function SupplierPicker({ value, onChange }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleOpenList = () => {
+    navigate("/suppliers?selectMode=true"); // navigate to supplier list in select mode
+  };
 
   return (
-    <EntityPicker
-      apiGet={() => dispatch(fetchSuppliers()).unwrap()}
-      apiAdd={(data) => dispatch(addSupplierThunk(data)).unwrap()}
-      label="Supplier"
-      {...props}
-    />
+    <div style={{ display: "flex", gap: 8, alignItems: "center" }}>
+      <EntityPicker
+        apiGet={() => dispatch(fetchSuppliers()).unwrap()}
+        apiAdd={(data) => dispatch(addSupplierThunk(data)).unwrap()}
+        label="Supplier"
+        value={value}
+        onChange={onChange}
+      />
+
+      <button type="button" onClick={handleOpenList}>
+        Select Supplier
+      </button>
+    </div>
   );
 }
