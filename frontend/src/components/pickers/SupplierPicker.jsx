fd1705007fe92f235035b068ae3aa08c9f5ee1ed
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-06 17:46:45 +0100

    updates to SupplierPicker.jsx

--- frontend/src/components/pickers/SupplierPicker.jsx
+++ frontend/src/components/pickers/SupplierPicker.jsx
@@ -1,13 +1,13 @@
 // src/components/pickers/SupplierPicker.jsx
 import React from "react";
 import EntityPicker from "./EntityPicker";
-import { getSuppliers, addSupplier } from "../../api/suppliers";
+import { getSuppliers, createSupplier } from "../../api/suppliers";
 
 export default function SupplierPicker(props) {
   return (
     <EntityPicker
       apiGet={getSuppliers}
-      apiAdd={addSupplier}
+      apiAdd={createSupplier}
       label="Supplier"
       {...props}
     />
