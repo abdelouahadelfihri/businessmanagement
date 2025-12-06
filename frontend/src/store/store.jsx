7874019d5d98464ed96ca007d1cdbb72281c8084
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-06 18:23:55 +0100

    updates to store.jsx

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import supplierReducer from '../slices/purchases/supplierSlice';
 import purchaseRequestReducer from '../slices/purchases/purchaseRequestSlice';
 import purchaseReceiptReducer from '../slices/purchases/purchaseReceiptSlice';
+import purchaseOrderReducer from '../slices/purchases/purchaseOrderSlice';
 
 export const store = configureStore({
   reducer: {
     suppliers: supplierReducer,
     purchaseRequest: purchaseRequestReducer,
-    purchaseReceipt: purchaseReceiptReducer
+    purchaseReceipt: purchaseReceiptReducer,
+    purchaseOrder: purchaseOrderReducer
   },
 });
