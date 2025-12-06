cb3af3903bd70120991cf4f7bce98e7a96cdd59e
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-06 19:07:13 +0100

    updates to PurchaseRequestPicker.jsx

--- frontend/src/components/pickers/PurchaseRequestPicker.jsx
+++ frontend/src/components/pickers/PurchaseRequestPicker.jsx
@@ -1,35 +1,18 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPurchaseRequests } from "../../slices/purchases/purchaseRequestSlice";
-import { useNavigate } from "react-router-dom";
+// src/components/pickers/PurchaseRequestPicker.jsx
+import React from "react";
+import { useDispatch } from "react-redux";
+import EntityPicker from "./EntityPicker";
+import { fetchPurchaseRequests, savePurchaseRequest } from "../../slices/purchases/purchaseRequestSlice";
 
-export default function PurchaseRequestPicker({ value, onChange }) {
+export default function PurchaseRequestPicker(props) {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const requests = useSelector((state) => state.purchaseRequest.list || []);
-
-  useEffect(() => {
-    if (!requests.length) dispatch(fetchPurchaseRequests());
-  }, [dispatch, requests]);
 
   return (
-    <div style={{ display: "flex", gap: 10, alignItems: "center" }}>
-      <select
-        value={value || ""}
-        onChange={(e) => onChange(e.target.value)}
-        style={{ flex: 1 }}
-      >
-        <option value="">-- Select a Purchase Request --</option>
-        {requests.map((r) => (
-          <option key={r.id} value={r.id}>
-            {`#${r.id} - ${r.title || r.description || "Request"}`}
-          </option>
-        ))}
-      </select>
-
-      <button type="button" onClick={() => navigate("/purchase-requests/add")}>
-        +
-      </button>
-    </div>
+    <EntityPicker
+      apiGet={() => dispatch(fetchPurchaseRequests()).unwrap()}
+      apiAdd={(data) => dispatch(savePurchaseRequest(data)).unwrap()} // create new request
+      label="Purchase Request"
+      {...props}
+    />
   );
 }
