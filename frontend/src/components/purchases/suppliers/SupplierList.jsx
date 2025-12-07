4d08b18eb6a45c68c90eccd583c51345e4ee2ed8
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-07 16:58:00 +0100

    updates to SupplierList.jsx

--- frontend/src/components/purchases/suppliers/SupplierList.jsx
+++ frontend/src/components/purchases/suppliers/SupplierList.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
-import { fetchSuppliers, setSupplier } from "../../slices/purchases/supplierSlice";
+import { fetchSuppliers, setSupplier } from "../../../slices/purchases/supplierSlice";
 
 export default function SupplierList() {
   const dispatch = useDispatch();
