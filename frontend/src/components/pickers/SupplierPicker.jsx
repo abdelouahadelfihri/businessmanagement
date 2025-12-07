3ec54c2eb503c5d2bab8d43fb85b1f8f83d5ba34
Abdelouahad <abdelouahadelfihri107@gmail.com>
2025-12-07 17:32:29 +0100

    updates to SupplierPicker.jsx

--- frontend/src/components/pickers/SupplierPicker.jsx
+++ frontend/src/components/pickers/SupplierPicker.jsx
@@ -1,12 +1,11 @@
 // src/components/pickers/SupplierPicker.jsx
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import EntityPicker from "./EntityPicker";
 import { fetchSuppliers, addSupplierThunk } from "../../slices/purchases/supplierSlice";
 
 export default function SupplierPicker({ value, onChange }) {
   const dispatch = useDispatch();
-  const suppliers = useSelector((state) => state.suppliers.list);
 
   return (
     <EntityPicker
